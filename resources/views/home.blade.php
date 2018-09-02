7f55ce9e2467acac232c127145e47b975c723412
Alexander Nakhleh <fv3491@wayne.edu>
2018-09-02 17:27:07 -0400

    orig, but doesn't work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,48 +8,36 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Your Todos</div>
 
-                <div class="panel-body">
-                    {{-- @include('inc.messages')
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                     --}}
-                    {{-- @endif --}}
+                <div class="">
                     <table class="table">
                         <tr>
-                            <td class="text-center"></td>
                             <td class="text-center">Priority</td>
                             <td class="text-center">Description</td>
+                            <td class="text-center"></td>
                             <td class="text-center"></td>
                         </tr>
                         <tr>
                             {!!Form::open(['action' => 'ListItemsController@store', 'method' => 'POST', 'class' => 'form-group'])!!}
-                            <td></td>
                             <td>{!!Form::number('priority','', ['class' => 'form-control'])!!}</td>
                             <td>{!!Form::text('item_desc','', ['class' => 'form-control'])!!}</td>
+                            <td></td>
                             <td>{!!Form::submit('Add', ['class' => 'btn btn-success'])!!}</td>
                             {!!Form::close()!!}
                         </tr>
                         @foreach($items as $item)
                                 <tr>
-                                    {!!Form::open(['action' => ['ListItemsController@destroy', $item->id], 'method' => 'POST', 'class' => 'form-group'])!!}
-                                    <td></td>
+                                    {!!Form::open(['action' => ['ListItemsController@update', $item->id], 'method' => 'POST', 'class' => 'form-group', 'id' => $item->id])!!}
+                                    {{Form::hidden('_method', 'PUT')}}
                                     <td class="text-center">{!!Form::number('priority',$item->priority, ['class' => 'form-control'])!!}</td>
                                     <td class="text-center">{!!Form::text('item_desc',$item->item_desc, ['class' => 'form-control'])!!}</td>
-                                    <td class="text-center">
-                                        {{Form::hidden('_method', 'DELETE')}}
-                                        {{Form::submit('Remove', ['class' => 'btn btn-danger'])}}
-                                    </td>
+                                    <td class="text-center">{{Form::submit('Update', ['class' => 'btn btn-info'])}}</td>
+                                    <td class="text-center">{{Form::submit('Remove', ['class' => 'btn btn-danger'])}}</td>
                                     {!!Form::close()!!}
                                 </tr>
                             @endforeach
                         </table>  
                     </table>
-
-                               
-                    
-                                   
+                        
                 </div>
             </div>
         </div>
