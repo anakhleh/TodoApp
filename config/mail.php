c34b3924c21821c88de47216899b9e527f68c824
Alexander Nakhleh <fv3491@wayne.edu>
2018-09-03 13:32:28 -0400

    Change reset-email 'from' field to 'TodoApp' from 'Example'

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'todoapp.resets@gmail.com',
+        'name' => 'TodoApp',
     ],
 
     /*
