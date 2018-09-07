7e450bdb3d4c3fb3e1b579dd25e9e429868b5e61
Alexander Nakhleh <fv3491@wayne.edu>
2018-09-07 00:13:54 -0400

    Try what is suggested in the laravel 5.4 guide for travis
    accidentally tried the 5.6 guide

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -31,7 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
     protected function driver()
     {
         return RemoteWebDriver::create(
-            'http://localhost:9232', DesiredCapabilities::chrome(), 5000, 10000
+            'http://localhost:9515', DesiredCapabilities::chrome(), 5000, 10000
         );
     }
 }
